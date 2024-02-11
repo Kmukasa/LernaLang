d915dd85a834860778a2fd5523c7b06463b030cf
keisha mukasa <keisha.mukasa@gmail.com>
2024-02-11 01:03:51 -0500

    fix: removed commented out code in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -25,26 +25,3 @@ const config = {
 };
 
 module.exports = mergeConfig(defaultConfig, config);
-
-// const { getDefaultConfig } = require("expo/metro-config");
-
-// module.exports = (() => {
-//   const config = getDefaultConfig(__dirname);
-
-//   config.resolver.resolverMainFields.unshift("sbmodern");
-
-//   const { transformer, resolver } = config;
-
-//   config.transformer = {
-//     ...transformer,
-//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
-//   };
-//   config.resolver = {
-//     ...resolver,
-//     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-//     sourceExts: [...resolver.sourceExts, "svg"],
-//     resolverMainFields: ["sbmodern", "react-native", "browser", "main"],
-//   };
-
-//   return config;
-// })();
